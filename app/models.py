3cd87ff9ad59094d526c3743300b07ff5573c9d3
jaeyeon <foodsksms@gmail.com>
2017-09-28 15:09:42 +0900

    Gravatar and Administrator

--- app/models.py
+++ app/models.py
@@ -5,7 +5,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import login_manager
 from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
+from datetime import datetime
+import hashlib
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -60,8 +62,17 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(64),unique=True,index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.INT,db.ForeignKey('roles.id'))
-    def __repr__(self):
-        return '<User %r>' % self.username
+    name =db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    # Ping
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+
 
         # password hashing
 
@@ -106,6 +117,28 @@ class User(UserMixin,db.Model):
                 self.role = Role.query.filter_by(Permission=0xff).first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(
+                self.email.endcode('utf-8')).hexdigest()
+
+    def change_email(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('change_email') != self.id:
+            return False
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        self.email = new_email
+        self.avatar_hash = hashlib.md5(
+            self.email.encode('utf-8')).hexdigest()
+        db.session.add(self)
+        return True
 
     # 사용자가 주어진 권한을 갖는지 평가
     def can(self, permissions):
@@ -113,6 +146,23 @@ class User(UserMixin,db.Model):
 
     def is_administrator(self):
         return self.can(Permission.ADMINISTER)
+
+    # Avatar
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'https://www.gravatar.com/avatar'
+        hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s{size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
+
+    avatar_hash = db.Column(db.String(32))
+
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
